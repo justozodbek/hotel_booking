ab73e86e3afab6c7cf1e88cd98af8b8cc0f745d1
Admin <@rustambekovozodbek09>
2025-06-30 17:54:34 +0500

    Добавил локализация и регион

--- hotel_booking/settings.py
+++ hotel_booking/settings.py
@@ -52,6 +52,14 @@ DATABASES = {
     }
 }
 
+LANGUAGE_CODE = 'ru'
+
+TIME_ZONE = 'Asia/Tashkent'
+
+USE_I18N = True
+
+USE_TZ = True
+
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
